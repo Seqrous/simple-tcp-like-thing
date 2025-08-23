967837b5c3dcd6a934314de618dee51f4a71ce9e
Krzysiek <krzysiek.ziach@gmail.com>
2025-08-23 21:29:52 +0200

    Finished handshake, added comments.

--- client.py
+++ client.py
@@ -13,10 +13,21 @@ PORT = 4000
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as conn:
     conn.settimeout(30.0)
     conn.bind((HOST, PORT))
+
+    # send SYN to the welcome socket
     seq_number = int(struct.unpack('I', randbytes(4))[0])  # 32bit int
     syn_datagram = Datagram(PORT, SERVER_PORT, seq_number, 0, True, False, False, False)
-    conn.sendto(syn_datagram.pack(), (SERVER_HOST, SERVER_PORT)) # SYN to welcome socket
+    conn.sendto(syn_datagram.pack(), (SERVER_HOST, SERVER_PORT))
+
+    # expect SYN-ACK from the welcome socket
     msg, addr = conn.recvfrom(1024)
     print(f"Got message from {addr}")
     datagram = Datagram.unpack(msg)
     print(f"{datagram=}")
+    if datagram.ack_number != seq_number + 1:
+        print("Unacked response from the server")
+        exit(0)
+
+    # send ACK to the newly assigned connection socket, increment server's sequence number
+    ack_datagram = Datagram(PORT, datagram.source_port, seq_number + 1, datagram.seq_number + 1, False, False, False, False)
+    conn.sendto(ack_datagram.pack(), (SERVER_HOST, datagram.source_port))
--- server.py
+++ server.py
@@ -17,6 +17,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as welcome_socket:
     try:
         while True:
             try:
+                # expect SYN from a client
                 msg, addr = welcome_socket.recvfrom(1024)
                 print(f"Got message from {addr}")
                 datagram = Datagram.unpack(msg)
@@ -25,10 +26,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as welcome_socket:
                 if datagram.syn:
                     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as conn:
                         conn.bind((HOST, 0))
+                        conn.settimeout(5.0)
                         _, conn_port = conn.getsockname()
-                        ack_number = int(struct.unpack('I', randbytes(4))[0])  # 32bit int
-                        ack_datagram = Datagram(conn_port, datagram.source_port, datagram.seq_number, ack_number, True, True, False, False)
-                        welcome_socket.sendto(ack_datagram.pack(), addr)
+
+                        # respond with SYN-ACK, increment client's sequence number
+                        seq_number = int(struct.unpack('I', randbytes(4))[0])  # 32bit int
+                        syn_ack_datagram = Datagram(conn_port, datagram.source_port, seq_number, datagram.seq_number + 1, True, False, False, False)
+                        welcome_socket.sendto(syn_ack_datagram.pack(), addr)
+
+                        # expect client's ACK
+                        ack_msg = conn.recv(1024)
+                        ack_datagram = Datagram.unpack(ack_msg)
+                        if not ack_datagram.syn and ack_datagram.ack_number == seq_number + 1:
+                            print("Handshake successful")
+                            continue
+
+                        print("Unacked response from the client")
 
             except socket.timeout:
                 continue
