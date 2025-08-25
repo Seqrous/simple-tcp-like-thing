1eac73a46d869f798cebae88a3a4269863ae5213
Krzysiek <krzysiek.ziach@gmail.com>
2025-08-25 20:36:36 +0200

    Introduced TCPConnection class.

--- tcp_connection.py
+++ tcp_connection.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import socket
 import struct
 from random import randbytes
@@ -15,7 +17,7 @@ class TCPConnector:
         self._seq_number: int = 0
         self._ack_number: int = 0
 
-    def connect(self, addr: tuple[str, int]) -> socket.socket:
+    def connect(self, addr: tuple[str, int]) -> TCPConnection:
         self._server_addr = addr
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.bind((self.host, self.port))
@@ -28,7 +30,7 @@ class TCPConnector:
             self._socket.close()
             raise
 
-        return self._socket
+        return TCPConnection(self._socket, self._server_addr, self._seq_number, self._ack_number)
 
     def _request_syn(self):
         print("SYN request")
@@ -59,7 +61,7 @@ class TCPConnector:
         syn_ack_datagram = Datagram.unpack(msg)
         print(f"{syn_ack_datagram=}")
 
-        self._socket.settimeout(None)
+        self._socket.settimeout(None)  # reset timeout
         if syn_ack_datagram.ack_number != self._seq_number:
             raise Exception("un-ACKed response from the server")
 
@@ -120,7 +122,7 @@ class TCPListener:
             print("Server's shutting down gracefully")
             self._wcm_socket.close()
 
-    def accept(self) -> socket.socket:
+    def accept(self) -> TCPConnection:
         self._conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._conn.bind((self.host, 0))  # assign random socket
         self._conn.settimeout(1.0)
@@ -143,6 +145,7 @@ class TCPListener:
 
         try:
             ack_msg = self._conn.recv(1024)
+            self._conn.settimeout(None)  # reset timeout
         except socket.timeout:
             raise Exception("Timeout waiting for ACK from the client")
 
@@ -150,6 +153,21 @@ class TCPListener:
         print(f"{ack_datagram=}")
         if not ack_datagram.syn and ack_datagram.ack and ack_datagram.ack_number == self._seq_number:
             print("Handshake successful")
-            return self._conn
+            return TCPConnection(self._conn, self._client_addr, self._seq_number, self._ack_number)
 
         raise Exception("Invalid ACK datagram received during handshake")
+
+
+class TCPConnection:
+
+    def __init__(self, sock: socket.socket, remote_addr: tuple[str, int], seq_number: int, ack_number: int):
+        self._socket = sock
+        self._rmt_addr = remote_addr
+        self._seq_number = seq_number
+        self._ack_number = ack_number
+
+    def send(self, data: bytes):
+        raise NotImplementedError()
+
+    def recv(self, buff_size: int) -> bytes:
+        raise NotImplementedError()
